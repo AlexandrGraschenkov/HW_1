68aed4cb83aab1fe2c82d55aa75475f3a2e9ce14
leonspok <igor9774@gmail.com>
2014-03-26 09:33:16 +0400

    task completed

--- HW_1/MYNotificationCenter.m
+++ HW_1/MYNotificationCenter.m
@@ -7,36 +7,96 @@
 //
 
 #import "MYNotificationCenter.h"
+#import "LPObjectSelectorPair.h"
 
-@interface MYNotificationCenter()
+@implementation MYNotificationCenter {
+    NSMutableDictionary *observers;
+}
 
-@end
++ (instancetype)sharedInstance {
+    static MYNotificationCenter *notificationCenter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        notificationCenter = [[MYNotificationCenter alloc] init];
+    });
+    return notificationCenter;
+}
 
-@implementation MYNotificationCenter
-+ (instancetype)sharedInstance
-{
-    return nil;
+- (id)init {
+    self = [super init];
+    if (self) {
+        observers = [NSMutableDictionary dictionary];
+    }
+    return self;
 }
 
 // можно только 1 раз подписаться обьекту на событие
-- (void)registerObject:(id)obj selector:(SEL)selector notificationName:(NSString*)name
-{
+- (void)registerObject:(id)obj selector:(SEL)selector notificationName:(NSString*)name {
+    if (!obj || !selector || name == (id)[NSNull null] || name.length == 0) {
+        return;
+    }
     
+    NSMutableSet *registeredObjects = nil;
+    if ([observers objectForKey:name] != nil) {
+        registeredObjects = [observers objectForKey:name];
+    } else {
+        registeredObjects = [NSMutableSet set];
+        [observers setObject:registeredObjects forKey:name];
+    }
+    
+    /*LPObjectSelectorPair *pair = [[LPObjectSelectorPair alloc] initWithObject:obj andSelector:selector];
+    [registeredObjects addObject:pair];*/
+    
+    BOOL found = NO;
+    for (LPObjectSelectorPair *registeredPair in registeredObjects) {
+        if (registeredPair.object == obj) {
+            found = YES;
+            break;
+        }
+    }
+    if (!found) {
+        LPObjectSelectorPair *pair = [[LPObjectSelectorPair alloc] initWithObject:obj andSelector:selector];
+        [registeredObjects addObject:pair];
+    }
 }
 
-- (void)unregisterObject:(id)obj notificationName:(NSString*)name
-{
-    
+- (void)unregisterObject:(id)obj notificationName:(NSString*)name {
+    if ([observers objectForKey:name]) {
+        NSMutableSet *registeredObjects = [observers objectForKey:name];
+        
+        LPObjectSelectorPair *pairToRemove = nil;
+        for (LPObjectSelectorPair *pair in registeredObjects) {
+            if (pair.object == obj) {
+                pairToRemove = pair;
+                break;
+            }
+        }
+        
+        
+        [registeredObjects removeObject:pairToRemove];
+    }
 }
 
-- (void)unregisterObject:(id)obj
-{
+- (void)unregisterObject:(id)obj {
+    if (!obj) {
+        return;
+    }
     
+    NSArray *notificationNames = [observers allKeys];
+    for (NSString *name in notificationNames) {
+        [self unregisterObject:obj notificationName:name];
+    }
 }
 
-- (void)postNotificationWithName:(NSString*)name
-{
-    
+- (void)postNotificationWithName:(NSString*)name {
+    if ([observers objectForKey:name]) {
+        NSArray *registeredObjects = [observers objectForKey:name];
+        for (LPObjectSelectorPair *pair in registeredObjects) {
+            if ([pair.object respondsToSelector:pair.selector]) {
+                ((void (*)(id, SEL))[pair.object methodForSelector:pair.selector])(pair.object, pair.selector);
+            }
+        }
+    }
 }
 
 @end
