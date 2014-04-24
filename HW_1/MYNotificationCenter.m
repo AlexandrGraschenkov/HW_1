371df959fc07e667ba0a06460bc26612583688f8
nastya13 <nastya94-94.08@mail.ru>
2014-04-24 16:17:20 -0700

    done

--- HW_1/MYNotificationCenter.m
+++ HW_1/MYNotificationCenter.m
@@ -7,36 +7,78 @@
 //
 
 #import "MYNotificationCenter.h"
+#import "ObjectSelector.h"
 
 @interface MYNotificationCenter()
+
+@property (strong, nonatomic) NSMutableDictionary *allNotifications;
 
 @end
 
 @implementation MYNotificationCenter
+
 + (instancetype)sharedInstance
 {
-    return nil;
+    static MYNotificationCenter *myNotification;
+    
+    if (!myNotification) {
+        myNotification = [[MYNotificationCenter alloc]init];
+        myNotification.allNotifications = [[NSMutableDictionary alloc]init];
+    }
+    
+    return myNotification;
 }
 
-// можно только 1 раз подписаться обьекту на событие
 - (void)registerObject:(id)obj selector:(SEL)selector notificationName:(NSString*)name
 {
+    
+    if (![_allNotifications objectForKey:name])
+        
+    {
+        NSMutableDictionary *newNotif = [[NSMutableDictionary alloc] init];
+        [_allNotifications setObject:newNotif forKey:name];
+    }
+    NSMutableDictionary *newNotif = [_allNotifications objectForKey:name];
+    NSValue *key = [NSValue valueWithNonretainedObject:obj];
+    [newNotif setObject:[[ObjectSelector alloc] initWithObject:obj andWithSelector:selector] forKey:key];
     
 }
 
 - (void)unregisterObject:(id)obj notificationName:(NSString*)name
 {
-    
+
+    NSMutableDictionary *dic = [_allNotifications objectForKey:name];
+ 	
+    NSValue *key = [NSValue valueWithNonretainedObject:obj];
+ 	
+    [dic removeObjectForKey:key];
 }
 
 - (void)unregisterObject:(id)obj
 {
-    
+    NSArray *keys = [_allNotifications allKeys];
+ 	
+    for (int i = 0; i < [keys count]; i++)
+        
+    {
+        NSMutableDictionary *dic = [_allNotifications objectForKey:keys[i]];
+        NSValue *key = [NSValue valueWithNonretainedObject:obj];
+        [dic removeObjectForKey:key];
+        
+    }
 }
 
 - (void)postNotificationWithName:(NSString*)name
 {
+
+    NSMutableDictionary *notif = [_allNotifications valueForKey:name];
+    NSArray *osps = [notif allValues];
     
+    for (int i = 0; i < [osps count]; i++)
+    {
+        ObjectSelector *pair = osps[i];
+        [[pair getObject] performSelector:[pair getSelector]];
+    }
 }
 
 @end
